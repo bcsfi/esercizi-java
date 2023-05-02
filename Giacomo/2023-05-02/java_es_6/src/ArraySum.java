f4c4a677f2770d86b58ae8f601284a21de61a490
Giacomo <giacomo.arcangioli@bcsoft.net>
2023-05-02 11:11:18 +0200

    primo commit di Giacomo

---
+++ Giacomo/2023-05-02/java_es_6/src/ArraySum.java
@@ -0,0 +1,26 @@
+import java.util.Vector;
+
+public class ArraySum {
+    static int arrayStringSum(Vector<String> someWord ){
+
+        int sum = 0;
+
+        for(int a=0; a < someWord.size()-1; a++)
+        {
+            System.out.print("Lunghezza vettore: "+  someWord.size());
+            String captureWord = someWord.get(a);
+
+            boolean verifica = Character.isUpperCase(captureWord.charAt(0));
+            if(verifica)
+            {
+                System.out.println(captureWord + "is capitalized.");
+                sum += captureWord.length();
+            }
+
+        }
+
+        System.out.println(someWord);
+        return sum;
+    }
+
+}
---
+++ Giacomo/2023-05-02/java_es_6/src/Main.java
@@ -0,0 +1,53 @@
+import java.sql.Array;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.Vector;
+
+class Main {
+    public static void main(String[] args)
+    {
+
+        // Vettore dove si andranno a salvare le diverse stringhe.
+        Vector<String> someWord = new Vector<String>();
+
+        // Dichiare l'oggetto scan come scanner del terminale.
+        Scanner scan = new Scanner(System.in);
+
+
+        while(true)
+        {
+
+            System.out.print("Insert your string: ");
+
+            // Catturare la linea tramite l'oggetto scan
+            String captureString = scan.nextLine();
+
+            // Aggiungi la string al vettore
+            someWord.add(captureString);
+
+
+            // La regex torna true se l'input è vuoto oppure si trova -> "" -> inserito. Una volta true esce dal while.
+            if(captureString == "" || captureString.matches("[\"]+(\").*"))
+                {
+                    System.out.print("You kill the loop.");
+                    break;
+                }
+
+            // Aggiungi la stringa al vettore
+            ArraySum.arrayStringSum(someWord);
+
+
+
+        }
+
+
+        // Stampa lunghezza delle parole capitalized
+        int res = ArraySum.arrayStringSum(someWord);
+        System.out.print(res + " Totale conteggio");
+
+    }
+
+}
+
+
