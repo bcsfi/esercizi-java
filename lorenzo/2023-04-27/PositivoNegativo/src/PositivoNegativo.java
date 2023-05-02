beb1a7d9046b9d11b40e742031667502b1b4d6c1
Lorenzo Chini <lorenzo.chini@bcsoft.net>
2023-05-02 16:02:39 +0200

    Organizzate cartelle

---
+++ lorenzo/2023-04-27/PositivoNegativo/src/PositivoNegativo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PositivoNegativo {
+    public static void main(String[] args) {
+        userInput();
+
+    }
+
+    private static void userInput() {
+        System.out.print("Inserisci numeri: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String list = scanner.nextLine();
+
+
+    }
+
+}
