41cd1cbbadb3846a6c1dae0851dee725c3c58dd8
Gabriele <gabriele.tarchi@bcsoft.net>
2023-05-08 15:41:32 +0200

    aggiunto al primo esercizio il setOccorrenze e modificato il ciclo di aggiunta delle occorrenze

--- Gabriele/2023-05-08/Es1/src/net/bcsoft/es1/Main.java
+++ Gabriele/2023-05-08/Es1/src/net/bcsoft/es1/Main.java
@@ -1,17 +1,16 @@
 package net.bcsoft.es1;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int dimensioneLista1, dimensioneLista2;
-        int aggiunta, contatore = 0;
-        ArrayList <Integer> numeri1 = new ArrayList<Integer>();
-        ArrayList <Integer> numeri2 = new ArrayList<Integer>();
-        ArrayList <Integer> occorrenze = new ArrayList<Integer>();
+        int aggiunta;
+        List <Integer> numeri1 = new ArrayList<Integer>();
+        List <Integer> numeri2 = new ArrayList<Integer>();
+        List <Integer> listaOccorrenze = new ArrayList<Integer>();
+        Set<Integer> setOccorrenze = new HashSet<>();
 
         System.out.println("CREAZIONE PRIMA LISTA \n" );
         System.out.println("Quanti numeri vuoi inserire nella prima lista? ");
@@ -21,23 +20,44 @@ public class Main {
             aggiunta = input.nextInt();
             numeri1.add(aggiunta);
         }
+
+        System.out.println("CREAZIONE SECONDA LISTA \n");
         System.out.println("Quanti numeri vuoi inserire nella seconda lista? ");
         dimensioneLista2 = input.nextInt();
-        System.out.println("CREAZIONE SECONDA LISTA \n");
         for (int i = 0; i < dimensioneLista2; i++){
             System.out.println("Inserisci il numero da aggiungere: ");
             aggiunta = input.nextInt();
             numeri2.add(aggiunta);
         }
-
+        /*
         for (int i = 0; i < dimensioneLista1; i++){
-            if (Objects.equals(numeri1.get(i), numeri2.get(i))){
+            for (int j = 0; j < dimensioneLista2; j++){
+                if (Objects.equals(numeri1.get(i), numeri2.get(j))){
+                    occorrenze.add(numeri1.get(i));
+                    contatore++;
+                }
+            }
+        }
+
+        for(int i = 0; i < dimensioneLista2; i++){
+            if(numeri1.contains(numeri2.get(i))){
                 occorrenze.add(numeri1.get(i));
                 contatore++;
             }
         }
-        System.out.println("Lista1 contiene " + contatore + " elementi di lista 1: ");
-        for (Integer integer : occorrenze) {
+          */
+        for (Integer item : numeri2) {
+            if (numeri1.contains(item)) {
+                listaOccorrenze.add(item);
+                setOccorrenze.add(item);
+            }
+        }
+        System.out.println(" Con lista occorrenze. Lista1 contiene " + listaOccorrenze.size() + " elementi di lista 2: ");
+        for (Integer integer : listaOccorrenze) {
+            System.out.println(integer + "\n");
+        }
+        System.out.println(" Con set occorrenze. Lista1 contiene " + setOccorrenze.size() + " elementi di lista 2: ");
+        for (Integer integer : setOccorrenze) {
             System.out.println(integer + "\n");
         }
     }
