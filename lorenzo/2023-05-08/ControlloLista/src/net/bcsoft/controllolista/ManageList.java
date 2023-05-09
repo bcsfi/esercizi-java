e86eef4fdf653095622f4abc7cb8dfd286a95cdb
Lorenzo Chini <lorenzo.chini@bcsoft.net>
2023-05-09 13:11:41 +0200

    Aggiunto package a ControlloLista 2023-05-08

---
+++ lorenzo/2023-05-08/ControlloLista/src/net/bcsoft/controllolista/ManageList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ManageList {
+    public static ArrayList<Integer> getComparing(ArrayList<Integer> list1, ArrayList<Integer> list2){
+        ArrayList<Integer> returnList = new ArrayList<Integer>();
+
+        for (Integer i : list1) {
+            if(list2.contains(i)) returnList.add(i);
+        }
+        return returnList;
+    }
+
+    public static String getList2String(ArrayList<Integer> comparingList){
+        String comparingString;
+        comparingString = comparingList.toString()
+                .replace("[", "").replace("]", "");
+
+        return comparingString;
+    }
+}
