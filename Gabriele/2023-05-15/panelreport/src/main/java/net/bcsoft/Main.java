48767ba9ffe41bfca2506de0f4d2788896d7a6c4
Gabriele <gabriele.tarchi@bcsoft.net>
2023-05-16 14:59:37 +0200

    Prima versione dell'esercizio sul report dei pannelli solari

---
+++ Gabriele/2023-05-15/panelreport/src/main/java/net/bcsoft/Main.java
@@ -0,0 +1,67 @@
+package net.bcsoft;
+
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String  percorsoIniziale;
+        Map<String, Double> mappaPerProvincia = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci il path del file da cui importare i dati: ");
+        percorsoIniziale= input.nextLine();
+        File file = new File(percorsoIniziale);
+        leggiFile(file, mappaPerProvincia);
+    }
+
+    public static void leggiFile(File file, Map <String, Double> mappa){
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            while (br.ready()) {
+                String riga = br.readLine();
+                elaboraRiga(riga, mappa);
+            }
+            scriviMappaSuFile(mappa);
+        } catch (IOException e) {
+            System.out.println("ERRORE DI LETTURA FILE");
+        }
+    }
+
+    private static void elaboraRiga(String riga, Map <String, Double> mappa) {
+        String[] words = new String[0];
+        if (riga != null) {
+            words = riga.trim().split(";");
+            String provincia = words[1].toUpperCase();
+            Double importo = Double.parseDouble(words[2]);
+            mettiInMappa(provincia, importo, mappa);
+        }
+    }
+
+    private static void mettiInMappa(String provincia, Double importo, Map <String, Double> mappa ){
+        if(!mappa.containsKey(provincia)){
+            mappa.put(provincia, importo);
+        }
+        else{
+            mappa.put(provincia, mappa.get(provincia) + importo );
+        }
+    }
+
+    private static void scriviMappaSuFile (Map <String, Double> mappa ){
+        try {
+            FileWriter myWriter = new FileWriter("reportFinale.txt");
+            for (String chiave : mappa.keySet()) {
+                Double valore = mappa.get(chiave);
+                myWriter.write("Provincia:" + chiave + " Importo: " + valore + "\n");
+            }
+            myWriter.close();
+            System.out.println("File scritto correttamente.");
+        } catch (IOException e) {
+            System.out.println("ERRORE FILE NON SCRITTO.A");
+            e.printStackTrace();
+        }
+    }
+}
