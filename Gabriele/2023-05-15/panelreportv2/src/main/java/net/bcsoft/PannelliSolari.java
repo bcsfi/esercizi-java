c4a236e9bafabffe1efcf557985f72988db60512
Gabriele <gabriele.tarchi@bcsoft.net>
2023-05-17 10:39:07 +0200

    seconda versione dell'esercizio sui pannelli solari

---
+++ Gabriele/2023-05-15/panelreportv2/src/main/java/net/bcsoft/PannelliSolari.java
@@ -0,0 +1,8 @@
+package net.bcsoft;
+
+
+public class PannelliSolari {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Gabriele/2023-05-15/panelreportv2/src/main/java/net/bcsoft/ReportCreator.java
@@ -0,0 +1,5 @@
+package net.bcsoft;
+
+public class ReportCreator {
+
+}
