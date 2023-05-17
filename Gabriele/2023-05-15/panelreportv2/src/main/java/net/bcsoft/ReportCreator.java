3913d0c987ce900b7bef19b3adb9708cccafaa28
Gabriele <gabriele.tarchi@bcsoft.net>
2023-05-17 10:53:22 +0200

    aggiunte delle cose al Report Creator

--- Gabriele/2023-05-15/panelreportv2/src/main/java/net/bcsoft/ReportCreator.java
+++ Gabriele/2023-05-15/panelreportv2/src/main/java/net/bcsoft/ReportCreator.java
@@ -1,5 +1,21 @@
 package net.bcsoft;
 
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReportCreator {
+    private Map < String, Double> mappaPerProvincia = new HashMap<>();
+    private Map <String, Double> mappaPerData = new HashMap<>();
+
+    public ReportCreator(Map < String, Double> mappaPerProvincia, Map <String, Double> mappaPerData){
+        this.mappaPerData = mappaPerData;
+        this.mappaPerProvincia = mappaPerProvincia;
+    }
+
+    public void leggiFile(String pathIniziale){
+        Path path = Path.of(pathIniziale);
+
+    }
 
 }
