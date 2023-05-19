9a21bc59b37c3478c7468012d9e8ec8565d72151
Lorenzo Chini <lorenzo.chini@bcsoft.net>
2023-05-19 14:27:32 +0200

    Pannelli solari versione 1.0 (per eseguibile) Gabriele Tarchi, Lorenzo Chini

--- Gabriele/2023-05-15/panelreportv2/src/main/java/net/bcsoft/panelreport/PannelliSolari.java
+++ Gabriele/2023-05-15/panelreportv2/src/main/java/net/bcsoft/panelreport/PannelliSolari.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 
 public class PannelliSolari {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        String pathIniziale = args[0];
+        String pathFinale = args[1];
 
+        /*
+        Scanner input = new Scanner(System.in);
         System.out.print("Inserire il path del file iniziale: ");
         String pathIniziale = input.next();
 
         System.out.print("Inserire il path dove creare i file: ");
         String pathFinale = input.next();
+        */
 
         try {
             ReportCreator report = new ReportCreator(pathIniziale, pathFinale);
