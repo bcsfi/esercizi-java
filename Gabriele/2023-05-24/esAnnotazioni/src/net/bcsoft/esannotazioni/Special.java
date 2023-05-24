49f9b3670eb93ab8509ccb1648702d710d711d5e
Gabriele <gabriele.tarchi@bcsoft.net>
2023-05-24 15:20:02 +0200

    esercizio annotazioni

---
+++ Gabriele/2023-05-24/esAnnotazioni/src/net/bcsoft/esannotazioni/Special.java
@@ -0,0 +1,9 @@
+package net.bcsoft.esannotazioni;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Special {
+}
