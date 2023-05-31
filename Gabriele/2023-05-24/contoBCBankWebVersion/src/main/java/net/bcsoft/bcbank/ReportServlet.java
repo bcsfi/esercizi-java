8fa073dec1f87ba3ad5c9887ad387befb6142f20
Gabriele <gabriele.tarchi@bcsoft.net>
2023-05-31 17:43:08 +0200

    BCBANK | tolto la classe program e sistemato il ReportServlet

--- Gabriele/2023-05-24/contoBCBankWebVersion/src/main/java/net/bcsoft/bcbank/ReportServlet.java
+++ Gabriele/2023-05-24/contoBCBankWebVersion/src/main/java/net/bcsoft/bcbank/ReportServlet.java
@@ -1,17 +1,39 @@
-package net.bcsoft.bcbank.util;
+package net.bcsoft.bcbank;
 
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import net.bcsoft.bcbank.model.ContoCorrente;
 import net.bcsoft.bcbank.model.EstrattoContoMensile;
 import net.bcsoft.bcbank.model.Transazione;
+import net.bcsoft.bcbank.util.DatabaseManager;
+import net.bcsoft.bcbank.util.Query;
+import net.bcsoft.bcbank.util.ReportCreator;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
-public class Program {
+@WebServlet(name = "Report", urlPatterns = "/report")
+public class ReportServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/plain");
+        PrintWriter writer = null;
+        String output = null;
+        try { 
+            writer = response.getWriter();
+            output = createStringReport();
+        } catch (IOException e) {
+            output = "ERRORE: " + e.getMessage();
+        }
+        writer.println(output);
+        writer.flush();
+    }
 
-    public static String start() {
+    private String createStringReport() {
         DatabaseManager database = null;
         String output = null;
 
@@ -35,7 +57,7 @@ public class Program {
         return output;
     }
 
-    private static void closeDatabaseConnection(DatabaseManager database) {
+    private void closeDatabaseConnection(DatabaseManager database) {
         try {
             database.closeConnection();
         } catch (SQLException e) {
@@ -43,5 +65,4 @@ public class Program {
             throw new RuntimeException(e);
         }
     }
-
 }
