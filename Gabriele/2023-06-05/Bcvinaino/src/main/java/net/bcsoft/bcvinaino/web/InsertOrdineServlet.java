3962136eb19a5d90f09c93f9db276bf8bc2b229a
Lorenzo Chini <lorenzo.chini@bcsoft.net>
2023-06-06 17:52:35 +0200

    BCVinaino 1.1 | Creata cartella 'resources' in main e 'insert.html', creata servlet 'InsertOrdineServlet' (non completa)

---
+++ Gabriele/2023-06-05/Bcvinaino/src/main/java/net/bcsoft/bcvinaino/web/InsertOrdineServlet.java
@@ -0,0 +1,14 @@
+package net.bcsoft.bcvinaino.web;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "InsertOrdine", urlPatterns = "/insertordine")
+public class InsertOrdineServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
+
