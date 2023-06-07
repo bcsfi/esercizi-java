1810be79f2d073bd9b250300e173c42e0a72e705
Lorenzo Chini <lorenzo.chini@bcsoft.net>
2023-06-07 11:53:02 +0200

    BC Vinaino 1.1 | implementato 'doPost' (vaaributi me'nu_id e' qu'antitÃÃa') n 'InsertOrdineServlet' e aggiunto file statico 'insert.html'

--- Gabriele/2023-06-05/Bcvinaino/src/main/java/net/bcsoft/bcvinaino/web/InsertOrdineServlet.java
+++ Gabriele/2023-06-05/Bcvinaino/src/main/java/net/bcsoft/bcvinaino/web/InsertOrdineServlet.java
@@ -4,11 +4,45 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import net.bcsoft.bcvinaino.util.DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet(name = "InsertOrdine", urlPatterns = "/insertordine")
 public class InsertOrdineServlet extends HttpServlet {
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    private Short menu_id;
+    private Integer quantita;
 
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        menu_id = Short.valueOf(request.getParameter("menu_id"));
+        quantita = Integer.valueOf(request.getParameter("quantita"));
+    }
+
+    private void insert() {
+        DatabaseManager database = null;
+        try {
+            database = new DatabaseManager("localhost", 5432,
+                    "bcvinaino", "postgres", "admin");
+            Connection connessioneDatabase = database.getConnection();
+
+
+        } catch (SQLException | ClassNotFoundException exception) {
+            System.out.println("ERRORE GENERICO | " + exception.getMessage());
+            throw new RuntimeException(exception);
+        } finally {
+            closeDatabaseConnection(database);
+        }
+    }
+
+    private void closeDatabaseConnection(DatabaseManager database) {
+        try {
+            database.closeConnection();
+        } catch (SQLException e) {
+            System.out.println("ERRORE DISCONNESSIONE DATABASE | " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 }
 
