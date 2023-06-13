75fe433b835525aae76e6c74f84e82f3ee4154d1
Gabriele <gabriele.tarchi@bcsoft.net>
2023-06-13 11:34:05 +0200

    creato il progetto spring

--- Gabriele/2023-06-05/Bcvinaino/src/main/java/net/bcsoft/bcvinaino/web/IncassiSettimanaliServlet.java
+++ Gabriele/2023-06-05/Bcvinaino/src/main/java/net/bcsoft/bcvinaino/web/IncassiSettimanaliServlet.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
