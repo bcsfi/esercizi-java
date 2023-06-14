0ead892f46e07cd0b178e18f2e597f174ff11e65
Lorenzo Chini <lorenzo.chini@bcsoft.net>
2023-06-14 16:01:35 +0200

    BCVinaino 1.3 | Progetto personale

---
+++ lorenzo/2023-06-13/bcvinaino/src/main/java/net/bcsoft/BcVinaino/BcVinainoApplication.java
@@ -0,0 +1,13 @@
+package net.bcsoft.BcVinaino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcVinainoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcVinainoApplication.class, args);
+	}
+
+}
