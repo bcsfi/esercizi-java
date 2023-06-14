21f32fd03d616418bfc0c4c8a9dc643d6db164f6
Gabriele <gabriele.tarchi@bcsoft.net>
2023-06-14 17:54:30 +0200

    primo commit

--- Gabriele/2023-06-13/BcVinaino/src/main/java/net/bcsoft/BcVinaino/controller/ReportController.java
+++ Gabriele/2023-06-13/BcVinaino/src/main/java/net/bcsoft/BcVinaino/controller/ReportController.java
@@ -1,8 +1,30 @@
 package net.bcsoft.bcvinaino.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.annotation.WebServlet;
+import org.apache.coyote.Request;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ReportController {
     //TODO creare tanti metodi quante servlet avevamo e mettere l'annotazione che si reputa corretta
+    @RequestMapping(value = "/focacce/{param1}", method = RequestMethod.GET)
+    public String focaccia(@PathVariable("param1") String param1){
+        return param1;
+    }
+
+    @RequestMapping(value = "/incassi", method = RequestMethod.GET )
+    public String incassi(){
+        return "incassi";
+    }
+
+    @RequestMapping(value = "/ordini", method = RequestMethod.GET)
+    public String ordini(){
+        return "ordini";
+    }
+
+    @RequestMapping(value = "/ordinisoglia", method = RequestMethod.GET)
+    public String ordiniSoglia(){
+        return "ordiniSoglia";
+    }
+
 }
