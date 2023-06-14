e9d928f7ab1e590b7aaf93566215d59f3b001f4d
Gabriele <gabriele.tarchi@bcsoft.net>
2023-06-14 16:13:22 +0200

    creato nuovo branch

---
+++ Gabriele/2023-06-13/BcVinaino/src/main/java/net/bcsoft/BcVinaino/controller/OrdiniController.java
@@ -0,0 +1,8 @@
+package net.bcsoft.bcvinaino.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrdiniController {
+
+}
---
+++ Gabriele/2023-06-13/BcVinaino/src/main/java/net/bcsoft/BcVinaino/controller/ReportController.java
@@ -0,0 +1,8 @@
+package net.bcsoft.bcvinaino.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+    //TODO creare tanti metodi quante servlet avevamo e mettere l'annotazione che si reputa corretta 
+}
