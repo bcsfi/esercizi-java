4ad0d868ae12b067d0f87f0725c890fed687f597
Gabriele <gabriele.tarchi@bcsoft.net>
2023-07-07 11:00:37 +0200

    aggiunto un admin

--- EserciziComuni/2023-06-26/bcvinaino/src/main/java/net/bcsoft/bcvinaino/config/SecurityConfig.java
+++ EserciziComuni/2023-06-26/bcvinaino/src/main/java/net/bcsoft/bcvinaino/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.*;
 
@@ -22,6 +28,22 @@ public class SecurityConfig {
                 .formLogin(withDefaults())
                 .build();
 
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
